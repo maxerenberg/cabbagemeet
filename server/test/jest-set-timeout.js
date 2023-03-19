0f73e1e25eb862ad9cdd376d086258e22412a80c
Max Erenberg <>
2023-03-19 13:49:04 -0400

    increase DB timeout in tests

--- server/test/jest-set-timeout.js
+++ server/test/jest-set-timeout.js
@@ -1,4 +1,4 @@
 // Creating and initializing a new database can take a long time if using
 // network-attached storage.
 // Increase this if necessary (milliseconds).
-jest.setTimeout(30000);
+jest.setTimeout(10000);
